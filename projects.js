e7a01904e9ea1ae0a72d328bc059a8f111ad3342
kebele <51454858+kebele@users.noreply.github.com>
2020-11-15 21:01:59 +0300

    update

--- projects.js
+++ projects.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
   const proje = document.querySelector(".proje");
-  const dugme = document.querySelector(".dugme");
+  
 
   let projects = [
     {
@@ -11,7 +11,8 @@ document.addEventListener("DOMContentLoaded", () => {
       icon: "fab fa-vuejs",
       color: "#41B883",
       background : "info",
-      flip : "flip-left"
+      flip : "flip-left",
+      inspire : "inspired"
     },
     {
       tech: "html",
@@ -21,7 +22,8 @@ document.addEventListener("DOMContentLoaded", () => {
       icon: "fab fa-css3",
       color: "#df4759",
       background : "danger",
-      flip : "flip-down"
+      flip : "flip-down",
+      inspire : "inspired"
     },
     {
       tech: "vue",
@@ -31,7 +33,8 @@ document.addEventListener("DOMContentLoaded", () => {
       icon: "fab fa-vuejs",
       color: "#41B883",
       background : "info",
-      flip : "flip-right"
+      flip : "flip-right",
+      inspire : "inspired"
     },
     {
       tech: "react",
@@ -41,7 +44,8 @@ document.addEventListener("DOMContentLoaded", () => {
       icon: "fab fa-react",
       color: "#61DBFB",
       background : "primary",
-      flip : "flip-left"
+      flip : "flip-left",
+      inspire : "inspired"
     },
     {
       tech: "js",
@@ -49,9 +53,10 @@ document.addEventListener("DOMContentLoaded", () => {
       code: "https://github.com/kebele/meal_finder",
       linkx: "https://kebelemealfinder.netlify.app/",
       icon: "fab fa-js",
-      color: "#f0db4f",
+      color: "#34495e",
       background : "secondary",
-      flip : "flip-up"
+      flip : "flip-up",
+      inspire : "partially inspired"
     },
     {
       tech: "vue",
@@ -61,7 +66,8 @@ document.addEventListener("DOMContentLoaded", () => {
       icon: "fab fa-vuejs",
       color: "#41B883",
       background : "info",
-      flip : "zoom-in"
+      flip : "zoom-in",
+      inspire : "inspired"
     },   
     {
       tech: "react",
@@ -71,17 +77,19 @@ document.addEventListener("DOMContentLoaded", () => {
       icon: "fab fa-react",
       color: "#61DBFB",
       background : "primary",
-      flip : "fade-left"
+      flip : "fade-left",
+      inspire : "inspired"
     },    
     {
       tech: "js",
-      name: "todo with category",
+      name: "todo",
       code: "https://github.com/kebele/to_do_march",
       linkx: "https://kebeletodo.netlify.app/",
       icon: "fab fa-js",
-      color: "#f0db4f",
+      color: "#34495e",
       background : "secondary",
-      flip : "flip-up"
+      flip : "flip-up",
+      inspire : "inspired"
     },
     {
       tech: "react",
@@ -91,7 +99,8 @@ document.addEventListener("DOMContentLoaded", () => {
       icon: "fab fa-react",
       color: "#61DBFB",
       background : "primary",
-      flip : "fade-up-right"
+      flip : "fade-up-right",
+      inspire : "inspired"
     },
     {
       tech: "html",
@@ -101,7 +110,8 @@ document.addEventListener("DOMContentLoaded", () => {
       icon: "fab fa-css3",
       color: "#df4759",
       background : "danger",
-      flip : "fade-down-right"
+      flip : "fade-down-right",
+      inspire : "inspired"
     },
     {
       tech: "js",
@@ -109,9 +119,10 @@ document.addEventListener("DOMContentLoaded", () => {
       code: "https://github.com/kebele/painty",
       linkx: "https://paintykebele.netlify.app/",
       icon: "fab fa-js",
-      color: "#f0db4f",
+      color: "#34495e",
       background : "secondary",
-      flip : "flip-up"
+      flip : "flip-up",
+      inspire : "unique"
     },
     {
       tech: "js",
@@ -119,9 +130,10 @@ document.addEventListener("DOMContentLoaded", () => {
       code: "https://github.com/kebele/snake",
       linkx: "https://kebelesnake.netlify.app/",
       icon: "fab fa-js",
-      color: "#f0db4f",
+      color: "#34495e",
       background : "secondary",
-      flip : "flip-up"
+      flip : "flip-up",
+      inspire : "mostly unique"
     },
     {
       tech: "react",
@@ -131,7 +143,8 @@ document.addEventListener("DOMContentLoaded", () => {
       icon: "fab fa-react",
       color: "#61DBFB",
       background : "primary",
-      flip : "flip-down"
+      flip : "flip-down",
+      inspire : "partally inspired"
     },
     {
       tech: "js",
@@ -139,9 +152,10 @@ document.addEventListener("DOMContentLoaded", () => {
       code: "https://github.com/kebele/breakout",
       linkx: "https://kebelebreakout.netlify.app/",
       icon: "fab fa-js",
-      color: "#f0db4f",
+      color: "#34495e",
       background : "secondary",
-      flip : "flip-up"
+      flip : "flip-up",
+      inspire : "inspired"
     },
     {
       tech: "js",
@@ -149,9 +163,10 @@ document.addEventListener("DOMContentLoaded", () => {
       code: "https://github.com/kebele/koltuk-secimi",
       linkx: "https://kebelekoltuksecimi.netlify.app/",
       icon: "fab fa-js",
-      color: "#f0db4f",
+      color: "#34495e",
       background : "secondary",
-      flip : "flip-up"
+      flip : "flip-up",
+      inspire : "inspired"
     },
     {
       tech: "react",
@@ -161,7 +176,8 @@ document.addEventListener("DOMContentLoaded", () => {
       icon: "fab fa-react",
       color: "#61DBFB",
       background : "primary",
-      flip : "flip-up"
+      flip : "flip-up",
+      inspire : "partially inspired"
     },
     {
       tech: "d3",
@@ -169,39 +185,373 @@ document.addEventListener("DOMContentLoaded", () => {
       code: "https://github.com/kebele/doping-in-bicycle-sport",
       linkx: "https://doping-in-bicycle-sport.vercel.app/",
       icon: "fas fa-plus",
-      color: "#ffc107",
+      color: "#e67e22",
       background : "warning",
-      flip : "zoom-in-down"
+      flip : "zoom-in-down",
+      inspire : "partially inspired"
     },
-    
+    {
+      tech: "js",
+      name: "wack a mole",
+      code: "https://github.com/kebele/wack_a_mole",
+      linkx: "https://kebelewackamole.netlify.app/",
+      icon: "fab fa-js",
+      color: "#2c3e50",
+      background : "warning",
+      flip : "zoom-in-down",
+      inspire : "inspired"
+    },
+    {
+      tech: "js",
+      name: "weather 2",
+      code: "https://github.com/kebele/weather",
+      linkx: "https://kebeleweather.netlify.app/",
+      icon: "fab fa-js",
+      color: "#34495e",
+      background : "warning",
+      flip : "zoom-in-down",
+      inspire : "inspired"
+    },
+    {
+      tech: "js",
+      name: "hangman",
+      code: "https://github.com/kebele/adam_asmaca",
+      linkx: "https://adamasma.netlify.app/",
+      icon: "fab fa-js",
+      color: "#34495e",
+      background : "warning",
+      flip : "zoom-in-down",
+      inspire : "inspired"
+    },
+    {
+      tech: "js",
+      name: "exchange rates",
+      code: "https://github.com/kebele/dovizkuru",
+      linkx: "https://kebeledovizkuru.netlify.app/",
+      icon: "fab fa-js",
+      color: "#34495e",
+      background : "warning",
+      flip : "zoom-in-down",
+      inspire : "inspired"
+    },
+    {
+      tech: "js",
+      name: "budget",
+      code: "https://github.com/kebele/budget",
+      linkx: "https://kebelebudget.netlify.app/",
+      icon: "fab fa-js",
+      color: "#34495e",
+      background : "warning",
+      flip : "zoom-in-down",
+      inspire : "inspired"
+    },
+    {
+      tech: "js",
+      name: "music player",
+      code: "https://github.com/kebele/music_player",
+      linkx: "https://kebelemusicplayer.netlify.app/",
+      icon: "fab fa-js",
+      color: "#34495e",
+      background : "warning",
+      flip : "zoom-in-down",
+      inspire : "inspired"
+    },
+    {
+      tech: "js",
+      name: "escape from boxes",
+      code: "https://github.com/kebele/escape_the_boxes",
+      linkx: "https://escapetheboxes.netlify.app/",
+      icon: "fab fa-js",
+      color: "#34495e",
+      background : "warning",
+      flip : "zoom-in-down",
+      inspire : "unique"
+    },
+    {
+      tech: "js",
+      name: "video player",
+      code: "https://github.com/kebele/video_player",
+      linkx: "https://kebelevideoplayer.netlify.app/",
+      icon: "fab fa-js",
+      color: "#34495e",
+      background : "warning",
+      flip : "zoom-in-down",
+      inspire : "mostly unique"
+    },
+    {
+      tech: "js",
+      name: "english sentences",
+      code: "https://github.com/kebele/engSent",
+      linkx: "https://engsent.netlify.app/",
+      icon: "fab fa-js",
+      color: "#34495e",
+      background : "warning",
+      flip : "zoom-in-down",
+      inspire : "mostly unique"
+    },
+    {
+      tech: "js",
+      name: "tap effects",
+      code: "https://github.com/kebele/tapEffects",
+      linkx: "https://tapeffects.netlify.app/",
+      icon: "fab fa-js",
+      color: "#34495e",
+      background : "warning",
+      flip : "zoom-in-down",
+      inspire : "mostly unique"
+    },
+    {
+      tech: "js",
+      name: "pong",
+      code: "https://github.com/kebele/pongy",
+      linkx: "https://pongy.netlify.app/",
+      icon: "fab fa-js",
+      color: "#34495e",
+      background : "warning",
+      flip : "zoom-in-down",
+      inspire : "inspired"
+    },
+    {
+      tech: "js",
+      name: "english words",
+      code: "https://github.com/kebele/engWords",
+      linkx: "https://engwords.netlify.app/",
+      icon: "fab fa-js",
+      color: "#34495e",
+      background : "warning",
+      flip : "zoom-in-down",
+      inspire : "inspired"
+    },
+    {
+      tech: "html",
+      name: "landing page x",
+      code: "https://github.com/kebele/landingX",
+      linkx: "https://landing-x.vercel.app/",
+      icon: "fab fa-css3",
+      color: "#df4759",
+      background : "danger",
+      flip : "fade-down-right",
+      inspire : "inspired"
+    },
+    {
+      tech: "html",
+      name: "landing page y",
+      code: "https://github.com/kebele/landingY",
+      linkx: "https://landing-y.vercel.app/",
+      icon: "fab fa-css3",
+      color: "#df4759",
+      background : "danger",
+      flip : "fade-down-right",
+      inspire : "inspired"
+    },
+    {
+      tech: "html",
+      name: "toggle sidebar",
+      code: "https://github.com/kebele/toggle_sidebar",
+      linkx: "https://toggle-sidebar.vercel.app/",
+      icon: "fab fa-css3",
+      color: "#df4759",
+      background : "danger",
+      flip : "fade-down-right",
+      inspire : "inspired"
+    },
+    {
+      tech: "html",
+      name: "animate svg",
+      code: "https://github.com/kebele/animate_svg2",
+      linkx: "https://animate-svg2.vercel.app/",
+      icon: "fab fa-css3",
+      color: "#df4759",
+      background : "danger",
+      flip : "fade-down-right",
+      inspire : "inspired"
+    },
+    {
+      tech: "html",
+      name: "animate svg",
+      code: "https://github.com/kebele/food_inc",
+      linkx: "https://food-inc.vercel.app/",
+      icon: "fab fa-css3",
+      color: "#df4759",
+      background : "danger",
+      flip : "fade-down-right",
+      inspire : "inspired"
+    },
+    {
+      tech: "html",
+      name: "site w",
+      code: "https://github.com/kebele/w2_bootstrap",
+      linkx: "https://w2-lovat.vercel.app/",
+      icon: "fab fa-css3",
+      color: "#df4759",
+      background : "danger",
+      flip : "fade-down-right",
+      inspire : "inspired"
+    },
+    {
+      tech: "html",
+      name: "site ws9",
+      code: "https://github.com/kebele/ws_9",
+      linkx: "https://ws-9-steel.vercel.app/",
+      icon: "fab fa-css3",
+      color: "#df4759",
+      background : "danger",
+      flip : "fade-down-right",
+      inspire : "inspired"
+    },
+    {
+      tech: "vue",
+      name: "kelimelik",
+      code: "https://github.com/kebele/vue_kelimelik",
+      linkx: "https://vue-kelimelik.vercel.app/",
+      icon: "fab fa-vuejs",
+      color: "#41B883",
+      background : "info",
+      flip : "zoom-in",
+      inspire : "inspired"
+    },   
+    {
+      tech: "vue",
+      name: "vinstagram",
+      code: "https://github.com/kebele/vue_instagram",
+      linkx: "https://vue-instagram-k.vercel.app/",
+      icon: "fab fa-vuejs",
+      color: "#41B883",
+      background : "info",
+      flip : "zoom-in",
+      inspire : "inspired"
+    },
+    {
+      tech: "js",
+      name: "jump-slide",
+      code: "https://github.com/kebele/jump_slide",
+      linkx: "https://jump-slide.vercel.app/",
+      icon: "fab fa-js",
+      color: "#34495e",
+      background : "warning",
+      flip : "zoom-in-down",
+      inspire : "inspired"
+    },   
+    {
+      tech: "html",
+      name: "4 dots",
+      code: "https://github.com/kebele/4_dots",
+      linkx: "https://4-dots.vercel.app/",
+      icon: "fab fa-css3",
+      color: "#df4759",
+      background : "danger",
+      flip : "fade-down-right",
+      inspire : "inspired"
+    },
+    {
+      tech: "html",
+      name: "3d rotating images",
+      code: "https://github.com/kebele/3D_rotating_img_glr",
+      linkx: "https://3-d-rotating-img-glr.vercel.app/",
+      icon: "fab fa-css3",
+      color: "#df4759",
+      background : "danger",
+      flip : "fade-down-right",
+      inspire : "inspired"
+    },
+    {
+      tech: "html",
+      name: "about progress bar",
+      code: "https://github.com/kebele/about_progress",
+      linkx: "https://about-progress.vercel.app/progress.html",
+      icon: "fab fa-css3",
+      color: "#df4759",
+      background : "danger",
+      flip : "fade-down-right",
+      inspire : "inspired"
+    },
+    {
+      tech: "html",
+      name: "clock",
+      code: "https://github.com/kebele/clock",
+      linkx: "https://clock-five.vercel.app/",
+      icon: "fab fa-css3",
+      color: "#df4759",
+      background : "danger",
+      flip : "fade-down-right",
+      inspire : "inspired"
+    },
+    {
+      tech: "html",
+      name: "animated eyes",
+      code: "https://github.com/kebele/animated-eyes",
+      linkx: "https://animated-eyes.vercel.app/",
+      icon: "fab fa-css3",
+      color: "#df4759",
+      background : "danger",
+      flip : "fade-down-right",
+      inspire : "inspired"
+    },
+    {
+      tech: "html",
+      name: "flashlight",
+      code: "https://github.com/kebele/flashlight",
+      linkx: "https://flashlight.vercel.app/",
+      icon: "fab fa-css3",
+      color: "#df4759",
+      background : "danger",
+      flip : "fade-down-right",
+      inspire : "inspired"
+    },
+    {
+      tech: "html",
+      name: "box model",
+      code: "https://github.com/kebele/resp_box_model",
+      linkx: "https://resp-box-model.vercel.app/",
+      icon: "fab fa-css3",
+      color: "#df4759",
+      background : "danger",
+      flip : "fade-down-right",
+      inspire : "inspired"
+    },
+    {
+      tech: "html",
+      name: "rotating cube",
+      code: "https://github.com/kebele/swiper_cube",
+      linkx: "https://resp-box-model.vercel.app/",
+      icon: "fab fa-css3",
+      color: "#df4759",
+      background : "danger",
+      flip : "fade-down-right",
+      inspire : "inspired"
+    },
+    {
+      tech: "js",
+      name: "dino game",
+      code: "https://github.com/kebele/dino_game",
+      linkx: "https://jump-slide.vercel.app/",
+      icon: "fab fa-js",
+      color: "#34495e",
+      background : "warning",
+      flip : "zoom-in-down",
+      inspire : "inspired"
+    }, 
+
   ];
 
   for (x of projects) {
-    proje.innerHTML += `
-    
-    <div class="row">
-      <div class="element-item ${x.tech} col-md-3 col-sm-6">          
-      <div class="card m-1">
-      <ul class="list-group list-group-flush">
-        <li class="list-group-item">
-          <span style="color:${x.color}"><i class="${x.icon} fa-2x"></i></span>
-          <span class="float-right"><a href="${x.code} target="_blank"><i class="fas fa-code"></i></a></span>
-        </li>
-        <li class="list-group-item">
-          <span style="font-size:1.1rem;"><a href="${x.linkx} target="_blank">${x.name}</a></span>
-        </li>
-      </ul>
-    </div>
-        </div>            
-      </div>  
-  `;
+    proje.innerHTML += 
+    `
+    <div class="element-item pro ${x.tech}">
+      <p class="name">${x.name}</p>
+      <p class="symbol" style="color:${x.color}"><i class="${x.icon}"></i></p>
+      <p class="weight"><a href="${x.code}" target="_blank">code</a></p>    
+      <p class="number"><a href="${x.linkx}" target="_blank">see</a></p>
+      <p class="info tooltip"><i class="fas fa-info"></i><span class="tooltiptext">${x.inspire}</span></p> 
+    </div>    
+  `
   }
   //   <img class="img-fluid" src="${x.img}"/>
 
-
-  var $grid2 = $(".grid2").isotope({
+   var $grid2 = $(".grid2").isotope({
     itemSelector: ".element-item",
     layoutMode: "fitRows",
+    // layoutMode: "packery",
     getSortData: {},
   });
 
