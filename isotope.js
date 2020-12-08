022af9969644cbc34882e00dcb43dac0b429caf2
kebele <51454858+kebele@users.noreply.github.com>
2020-12-08 16:31:16 +0300

    update

--- isotope.js
+++ isotope.js
@@ -1,14 +1,14 @@
-// init Isotope
+//init Isotope
 /* isotope js i nav-content e uygula demek
 */
 $('#nav-content').isotope({
     // options
   });
   // filter items on button click
-  $('#nav-filter').on( 'click', 'button', function() {
-    var filterValue = $(this).attr('data-filter');
-    $('#nav-content').isotope({ filter: filterValue });
-  });
+  // $('#nav-filter').on( 'click', 'button', function() {
+  //   var filterValue = $(this).attr('data-filter');
+  //   $('#nav-content').isotope({ filter: filterValue });
+  // });
 
   $(document).on('click', '[data-toggle="lightbox"]', function(event) {
     event.preventDefault();
